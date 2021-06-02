bfd97d0332d312d8f02e427be310dde6a23b088c
Gustavo Colombo <gusttavocolombo@gmail.com>
2021-06-02 20:16:33 -0300

    feature: transaction modal

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeimg from '../../assets/income.svg';
@@ -13,6 +13,17 @@ interface TransactionModalProps{
 export default function TransactionModal({isOpen, onRequestClose}: TransactionModalProps){
   
   const [typeButton, setTypeButton] = useState('deposit');
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+  function handleSubmitTransaction(event: FormEvent){
+    event.preventDefault();
+
+    console.log({
+      title, value, category
+    })
+  }
 
   return(
     <Modal
@@ -26,31 +37,46 @@ export default function TransactionModal({isOpen, onRequestClose}: TransactionMo
         <img src={closeImg} alt="fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleSubmitTransaction}>
         <h2>Cadastrar Transação</h2>
 
-        <input type="text" placeholder="Título"/>
-        <input type="number" placeholder="Valor"/>
+        <input type="text" 
+          placeholder="Título" 
+          value={title} 
+          onChange={event => setTitle(event.target.value)}/>
+
+        <input 
+          type="number" 
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
         
         <ContainerGrid>
-          <button type="button" 
+          <button 
+            type="button" 
             className={typeButton === 'deposit' ? 'active' : ''}
             onClick={()=> setTypeButton('deposit')}
-            >
+          >
             <img src={incomeimg} alt="entrada" />
             Entrada
           </button>
 
-          <button type="button" 
-          className={typeButton === 'withdraw' ? 'not-active' : ''}
-          onClick={()=> setTypeButton('withdraw')}
+          <button 
+            type="button" 
+            className={typeButton === 'withdraw' ? 'not-active' : ''}
+            onClick={()=> setTypeButton('withdraw')}
           >
             <img src={outcomeimg} alt="saida" />
             Saída
           </button>
         </ContainerGrid>
 
-        <input type="text" placeholder="Categoria" />
+        <input type="text" 
+          placeholder="Categoria" 
+          value={category} 
+          onChange={event => setCategory(event.target.value)}
+        />
 
         <button type="submit">
           Cadastrar
